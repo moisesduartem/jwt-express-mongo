4c03e07ee4921e22f6ae17dc7518913f658a77e4
Moisés Mariano <moisesduartem@gmail.com>
2020-12-10 11:11:58 -0300

    create users GET route, to list all users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
 import User from '../models/User';
 
 class UserController {
+    
+    async index(req, res) {
+        const users = await User.find();
+        return res.json(users || []);
+    },
+
     async store(req, res) {
 
         const { name, email, password } = req.body;
