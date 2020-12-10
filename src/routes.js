5e806acc617d1b765b692b28e046ce4163528806
Moisés Mariano <moisesduartem@gmail.com>
2020-12-10 10:56:43 -0300

    implements a login route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
+import AuthController from './controllers/AuthController';
 
 const routes = new Router();
 
+routes.post('/login', AuthController.store);
 routes.post('/register', UserController.store);
 
 export default routes;
