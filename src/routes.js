b9ce370fd2e54cba9ce8112dc38981afccb9b5e0
Moisés Mariano <moisesduartem@gmail.com>
2020-12-10 11:12:38 -0300

    create a route protected with auth middleware to list all users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import auth from './middlewares/auth';
 import UserController from './controllers/UserController';
 import AuthController from './controllers/AuthController';
 
 const routes = new Router();
 
+routes.get('/users', auth, UserController.index);
 routes.post('/login', AuthController.store);
 routes.post('/register', UserController.store);
 
